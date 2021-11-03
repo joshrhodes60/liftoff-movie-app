1f77c40d81398e1ebc5f2e788cdcaff716fddad9
“josh” <joshrhodes60@icloud.com>
2021-11-03 10:34:21 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -28,15 +28,21 @@ import React, { useEffect, useState } from "react";
 
 import Movie from "./components/Movie";
 
+// const FEATURED_API =
+//   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
 const FEATURED_API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  "https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=28";
 
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+// const GENRE_API = "https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&with_genres=28";
+
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(async () => {
     getMovies(FEATURED_API);
   }, []);
@@ -62,11 +68,29 @@ function App() {
     setSearchTerm(e.target.value);
   };
 
+  // const handleGenreFilter = (movie) => {
+  //   let i;
+  //   for(i = 0; i < movie.genre.length; i++) {
+  //     console.log(movies)
+  //     if(movie.genres[i].toLowerCase() === movie.toLowerCase()) {
+  //       console.log(movie.genres[i].title);
+  //       console.log(movie.genres[i].overview);
+  //       console.log(movie.genres[i].poster);
+  //     }
+  //   }
+  // }
+
+  // const Movies = movies && movies.map((movie) => {
+  //   return movie.map((movies)=> {
+  //     return handleGenreFilter(movies);
+  //   })
+  // })
+
   return (
     <>
-     
       <header>
-        <h1> MOVIE APP </h1>
+        <h1>&#128576; MOTION PICTURE MADNESS &#128576;</h1>
+
         <form onSubmit={handleOnSubmit}>
           <input
             className="search"
@@ -77,6 +101,7 @@ function App() {
           />
         </form>
       </header>
+
       <div className="movie-container">
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
